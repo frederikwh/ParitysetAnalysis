9186361a61170f322d5b828402da4a489ebecd78
Frederik Wedel-Heinen <frederikwedel-heinen@Frederiks-MacBook-Pro.local>
2016-10-12 21:41:25 +0200

    Initial Commit

---
+++ ParitysetRectangleAnalysis/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ParitysetRectangleAnalysis
+//
+//  Created by Frederik Wedel-Heinen on 10/12/16.
+//  Copyright © 2016 Frederik Wedel-Heinen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
